31cd33b86be8df37d7f8f1cd93e7f42361e031ef
Pivinter <70973362+Pivinter@users.noreply.github.com>
2022-10-14 20:29:55 +0300

    Add project files.

---
+++ TestLab55/main.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+double F(double x)
+{
+    double F;
+    F = sin(x) - cos(x);
+    return F;
+}
+void main()
+{
+    double a, b, eps;
+    int m,x ;
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "eps = "; cin >> eps;
+    if (eps<0,a < b,F(a) * F(b) < 0) {
+        if (F(a) < 0 && F(b)>0) {
+            cout << "True" << endl;
+            m = (a + b) / 2.0;
+                while (fabs(F(m)) > eps) {
+                        if (F(m) == 0) {
+                            x = m;
+                        }
+                        else {
+                            if (F(a) * F(m) < 0) {
+                                cout << a << "<=" << "x" << "<=" << m << endl ;
+                                break;
+                            }
+                            else {
+                                cout << m << "<=" << "x" << "<=" << b << endl;
+                                break;
+                            }
+                        }
+                }
+        }
+    }
+    else {
+        cout << "false"<< endl;
+    }
+}
